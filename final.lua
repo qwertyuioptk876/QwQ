2c380e64d89563f75dc33203cc05bb3af50bbc54
qwertyuioptk876 <wangyoucheng014@gmail.com>
2026-07-31 00:27:42 +0800

    Add place ID check before executing code

--- final.lua
+++ final.lua
@@ -3,3 +3,9 @@ local Localplayer = player.LocalPlayer
 
 local lobby_ID = 99521272836282
 local battle_ID = 79787558257549
+
+local currentplace = game.PlaceID
+if currentplace ~= battle_ID then
+  print("位置錯誤")
+  return
+end
